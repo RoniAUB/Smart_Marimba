47e4047dc3e18fda4297894b55f5df931e4805df
RoniAUB <157002892+RoniAUB@users.noreply.github.com>
2024-11-22 08:14:17 +0200

    Data_Acquisition_Low.ino

--- Data_Acquisition_Low.ino
+++ Data_Acquisition_Low.ino
@@ -1,1 +1,17 @@
-{\rtf1}
+#include <_Teensy.h>
+#include <stdio.h>
+
+void setup() {
+  // Initialize serial communication at 2000000 baud
+  Serial.begin(2000000);
+}
+
+void loop() {
+  // Read the analog value from pin A3 (value range 0-1023)
+  int sensorValue1 = analogRead(14);
+  int sensorValue2 = analogRead(18);
+
+  // Send the sensor values as 4-byte integers
+  Serial.write((uint8_t *)&sensorValue1, sizeof(sensorValue1));
+  Serial.write((uint8_t *)&sensorValue2, sizeof(sensorValue2));
+  }
